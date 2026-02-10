999febe98d0cb9fc05f336332b667318e4111dd6
Neurolius <ewgensafonow@gmail.com>
2026-02-10 19:58:47 +0800

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,17 @@ const react = require("@vitejs/plugin-react");
 
 module.exports = defineConfig({
     plugins: [react()],
+    esbuild: {
+        loader: "jsx",
+        include: /src\/.*\.js$/
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            loader: {
+                ".js": "jsx"
+            }
+        }
+    },
     test: {
         environment: "jsdom",
         setupFiles: "./src/setupTests.js"
